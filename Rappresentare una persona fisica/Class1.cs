c6bbf0d872a697c7017f4b07202591824c365f90
Sciusco.20197 <Sciusco.20197@E2-9-PC25>
2023-09-21 10:50:03 +0200

    Aggiungere i file di progetto.

---
+++ Rappresentare una persona fisica/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Rappresentare_una_persona_fisica
+{
+    internal class Persona
+    {
+        public int Codice;
+        public string Cognome;
+        public string Nome;
+        public bool Registrato;
+
+        public Persona(int codice, string cognome, string nome, bool registrato)
+        {
+            Codice = codice;
+            Cognome = cognome;
+            Nome = nome;
+            Registrato = false;
+        }
+        public void Registra()
+        {
+            Registrato = true;
+            Console.WriteLine($"{Cognome} {Nome} è stato registrato");
+        }
+
+        public void Mostra()
+        {
+
+            Console.WriteLine($"Codice: {Codice}");
+            Console.WriteLine($"Cognome: {Cognome}");
+            Console.WriteLine($"Nome: {Nome}");
+            Console.WriteLine($"Registrato: {(Registrato ? "Si" : "No")}");
+        }
+
+    }
+}
---
+++ Rappresentare una persona fisica/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using Rappresentare_una_persona_fisica;
+
+namespace Rappresentare_una_persona_fisica
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona1 = new Persona(1, "Rossi", "Mario");
+
+            Console.WriteLine("Dati prima ddella registrazione");
+            persona1.Mostra();
+
+            persona1.Registra();
+
+            Console.WriteLine("\nDati dopo la registrazione");
+            persona1.Mostra();
+
+        }
+    }
+}
